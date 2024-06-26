ee73b839c9df3df143c3392d3da30edea759523e
Stephen Cerruti <scerruti@otabi.com>
2024-06-26 14:19:47 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+// When you are ready to run unit tests, change the value below from N to Y
+// ReadyForSubmission=Y
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    //Invoke your static method here
+    
+
+  }
+  
+  //Create your static method here 
+  
+}
